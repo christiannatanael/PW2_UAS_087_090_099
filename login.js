fc77c415506bf2a8c004364b368a97539e6b5fb0
christian <natancns@gmail.com>
2023-12-21 22:56:08 +0700

    first commit

---
+++ login.js
@@ -0,0 +1,35 @@
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const wrapper = document.querySelector('.wrapper');
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+
+// elemen formulir sign-up
+const signUpForm = document.querySelector('.form-wrapper.sign-up form');
+
+// event listener untuk submit form signup
+signUpForm.addEventListener('submit', function (event) {
+
+  // ke login.html
+  window.location.href = 'login.html';
+
+  event.preventDefault();
+});
+
+// elemen formulir login
+const loginForm = document.querySelector('.form-wrapper.sign-in form');
+
+// event listener untuk submit form login
+loginForm.addEventListener('submit', function (event) {
+  // Tambahkan kode untuk memproses login di sini
+
+  // arahkan pengguna ke Mainpage/index.html
+  window.location.href = 'Mainpage/index.html';
+
+  // aksi default agar form tidak di-submit secara normal
+  event.preventDefault();
+});
